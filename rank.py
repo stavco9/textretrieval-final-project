3dc3660fafad37aa0ecd44bcaa6c8096b1d00ecc
Stav Cohen <stavco9@gmail.com>
2025-01-18 15:01:48 +0000

    Improved average map

--- rank.py
+++ rank.py
@@ -26,7 +26,7 @@ def rank_documents(run_number, method="bm25", stemmer="krovetz", top_k=1000):
     searcher.set_analyzer(analyzer)
 
     if method == "rm3":
-        searcher.set_rm3(fb_terms=20, fb_docs=50, original_query_weight=0.3)
+        searcher.set_rm3(fb_terms=100, fb_docs=3, original_query_weight=0.3)
     elif method == "bm25":
         searcher.set_bm25(k1=0.9, b=0.4)
     elif method == "qld":
@@ -51,6 +51,6 @@ def rank_documents(run_number, method="bm25", stemmer="krovetz", top_k=1000):
             for rank, (docid, _, score) in enumerate(hits, start=1):
                 f.write(f"{topic_id} Q0 {docid} {rank} {score:.4f} run{run_number}\n")
 
-#rank_documents(1, 'rm3')
+rank_documents(1, 'rm3')
 #rank_documents(2, 'bm25')
-rank_documents(3, 'qld')
+#rank_documents(3, 'qld')
