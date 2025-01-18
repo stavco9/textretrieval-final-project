017a55436cfda26091bb2b3f5d1f647cab62051e
Stav Cohen <stavco9@gmail.com>
2025-01-18 22:53:53 +0000

    Added lightGBM eval file

--- trec_eval.sh
+++ trec_eval.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_1_rm3.res > ./results/run_1_rm3_eval.txt
+#python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_1_rm3.res > ./results/run_1_rm3_eval.txt
 #python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_2_bm25.res > ./results/run_2_bm25_eval.txt
 #python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_3_qld.res > ./results/run_3_qld_eval.txt
+python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_4_lightgbm.res > ./results/run_4_lightgbm_eval.txt
