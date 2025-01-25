00487fb95672c4714213f9d8b4c6cc099234df9f
Stav Cohen <stavco9@gmail.com>
2025-01-25 16:17:45 +0000

    Added result without doc_num in the parameters

--- trec_eval.sh
+++ trec_eval.sh
@@ -4,5 +4,5 @@
 #python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_2_bm25.res > ./results/run_2_bm25_eval.txt
 #python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_3_qld.res > ./results/run_3_qld_eval.txt
 #python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_4_lightgbm.res > ./results/run_4_lightgbm_eval.txt
-python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_5_vector.res > ./results/run_5_vector_eval.txt
-#python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_4_lightgbm_pilot.res > ./results/run_4_lightgbm_eval_pilot.txt
+#python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_5_vector.res > ./results/run_5_vector_eval.txt
+python -m pyserini.eval.trec_eval -q ./files/qrels_50_Queries ./results/run_4_lightgbm_pilot.res > ./results/run_4_lightgbm_eval_pilot.txt
